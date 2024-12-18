3671e641f38a7a39cdebef6344adbc1e679853ec
mirabjerkhagen <94913911+mirabjerkhagen@users.noreply.github.com>
2024-12-18 14:03:07 +0100

    Update listView.tsx

--- Lab3/app/listView.tsx
+++ Lab3/app/listView.tsx
@@ -48,7 +48,7 @@ const styles = StyleSheet.create({
   container: { 
     flex: 1, 
     padding: 10, 
-    backgroundColor: 'black',
+    backgroundColor: 'white',
   },
   item: {
     padding: 10, 
